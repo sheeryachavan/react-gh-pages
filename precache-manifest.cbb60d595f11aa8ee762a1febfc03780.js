05e62812da57168da0a01cdc31d7d40f5edf391b
sheeryachavan <shreesh.chavan@gmail.com>
2019-10-07 19:47:38 -0400

    Updates

---
+++ precache-manifest.cbb60d595f11aa8ee762a1febfc03780.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b416a10a501250d3a0657ebe72f85ac",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "442e6611413066f6353f",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "78a24940739f277e3efc",
+    "url": "/react-gh-pages/static/js/2.5092a21c.chunk.js"
+  },
+  {
+    "revision": "442e6611413066f6353f",
+    "url": "/react-gh-pages/static/js/main.1332a616.chunk.js"
+  },
+  {
+    "revision": "5f8dec9dd65745d16996",
+    "url": "/react-gh-pages/static/js/runtime-main.88f605a9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
